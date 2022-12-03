12e3fd27cc8e267555685fcd1dd116557b215060
Max Matthews <max@mzmtech.com>
2022-12-03 12:02:58 -0500

    Add some comments

--- day3/part1.js
+++ day3/part1.js
@@ -6,12 +6,17 @@ let prioritySum = 0;
 
 for (const line of allLines) {
   const halfwayIndex = line.length / 2;
-  //split the leftHalf into a string so it's easier to iterate over
+  //split the leftHalf into an array so it's easier to iterate over
   const leftHalf = line.substring(0, halfwayIndex).split("");
   const rightHalf = line.substring(halfwayIndex);
 
+  //use each letter in the left half of the string to see if the right half
+  //includes that letter, then store the letter that matches
   const matchingLetter = leftHalf.find((letter) => rightHalf.includes(letter));
 
+  //this if statement could be written as a inline tenerary to determine the
+  //num to subtract, but I think the code is easier to read by leaving it as
+  //an if statement
   if (matchingLetter === matchingLetter.toUpperCase()) {
     //matching letter is uppercase
     //A is char code 65, so subtracting 65 from the letters uppercase char code
@@ -19,7 +24,7 @@ for (const line of allLines) {
     //gives us the matching priority number from the challenge
     prioritySum += matchingLetter.charCodeAt(0) - 65 + 27;
   } else {
-    //lowercase
+    //matching letter i lowercase
     //a is char code 97, so subtracting 96 from the letters lowercase char code
     //gives us the matching priority number from the challenge
     prioritySum += matchingLetter.charCodeAt(0) - 96;
--- day3/part2.js
+++ day3/part2.js
@@ -4,15 +4,23 @@ const allLines = fileContents.split(/\r?\n/);
 
 let prioritySum = 0;
 
+//loop over each group of elves by incrementing by three each loop run
+//the index representing the first elf number in each group
 for (let elfIndex = 0; elfIndex < allLines.length; elfIndex += 3) {
+  //split the firstElf into an array so it's easier to iterate over
   const firstElf = allLines[elfIndex].split("");
   const secondElf = allLines[elfIndex + 1];
   const thirdElf = allLines[elfIndex + 2];
 
+  //use each letter in the of firstElf to see if it's included in
+  //the second AND third elfs string
   const matchingLetter = firstElf.find(
     (letter) => secondElf.includes(letter) && thirdElf.includes(letter)
   );
 
+  //this if statement could be written as a inline tenerary to determine the
+  //num to subtract, but I think the code is easier to read by leaving it as
+  //an if statement
   if (matchingLetter === matchingLetter.toUpperCase()) {
     //matching letter is uppercase
     //A is char code 65, so subtracting 65 from the letters uppercase char code
@@ -20,7 +28,7 @@ for (let elfIndex = 0; elfIndex < allLines.length; elfIndex += 3) {
     //gives us the matching priority number from the challenge
     prioritySum += matchingLetter.charCodeAt(0) - 65 + 27;
   } else {
-    //lowercase
+    //matching letter i lowercase
     //a is char code 97, so subtracting 96 from the letters lowercase char code
     //gives us the matching priority number from the challenge
     prioritySum += matchingLetter.charCodeAt(0) - 96;
