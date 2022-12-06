0569cbc7a44cc0eb6ff8ce3269d1469de7e51cab
Max Matthews <max@mzmtech.com>
2022-12-06 11:50:54 -0500

    Second refactor

--- day6/part2Refactored.js
+++ day6/part2Refactored.js
@@ -7,22 +7,23 @@ const fileContents = fs.readFileSync("./input.txt", "utf-8");
 let lookAheadLength = 14; //make this a variable so we can adjust it if needed
 
 //loop over every letter in the input
-for (let i = 0; i < fileContents.length; i++) {
+letterLoop: for (let i = 0; i < fileContents.length; i++) {
   //build an array with the current letter and the next ${lookAheadLength=14} letters
   let letters = [];
 
   //push the current letter and 13 more into the array
   for (let lookAhead = i; lookAhead < i + lookAheadLength; lookAhead++) {
     if (letters.includes(fileContents[lookAhead])) {
-      continue;
+      continue letterLoop;
     } else {
       letters.push(fileContents[lookAhead]);
     }
   }
-  if (letters.length === lookAheadLength) {
-    console.log(i + lookAheadLength);
-    break;
-  }
+
+  //this won't run unless it gets through all 14 of the lookAhead letters due to the
+  //continue in that loop
+  console.log(i + lookAheadLength);
+  break;
 }
 
 //this is a millisecond faster than my initial solution
