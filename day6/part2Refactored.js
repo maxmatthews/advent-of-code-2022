18c0ffc97698552d1be78fc9256c369af89d8e56
Max Matthews <max@mzmtech.com>
2022-12-06 11:53:07 -0500

    More detailed comments

--- day6/part2Refactored.js
+++ day6/part2Refactored.js
@@ -14,14 +14,20 @@ letterLoop: for (let i = 0; i < fileContents.length; i++) {
   //push the current letter and 13 more into the array
   for (let lookAhead = i; lookAhead < i + lookAheadLength; lookAhead++) {
     if (letters.includes(fileContents[lookAhead])) {
+      //if the current lookAhead letter is already in the array, it must be
+      //a duplicate letter, so move onto the next starting letter in the
+      //outer loop
       continue letterLoop;
     } else {
+      //the letter doesn't exist yet, so push it into the array so we can check
+      //the next letter against it
       letters.push(fileContents[lookAhead]);
     }
   }
 
   //this won't run unless it gets through all 14 of the lookAhead letters due to the
-  //continue in that loop
+  //continue in that loop, if it does, we found our 14 unique characters and can stop
+  //the outer loop
   console.log(i + lookAheadLength);
   break;
 }
